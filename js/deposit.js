86c10e53f48bc810d49c7393509cc2c9509761e4
samro01 <srozario22@gmail.com>
2022-08-21 14:40:20 +0200

    first commit

---
+++ js/deposit.js
@@ -0,0 +1,6 @@
+document.getElementById('btn-deposit').addEventListener('click', function () {
+    const depositField = document.getElementById('deposit-field');
+    const newDepositFieldAmountString = depositField.value;
+   const newDepositAmount =parseFloat(newDepositFieldAmountString);
+
+})
